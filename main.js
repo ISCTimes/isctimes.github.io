79afe377981a1ec743cf77e6fe8772b7a4360517
ISCTimes <47945123+ISCTimes@users.noreply.github.com>
2019-03-02 01:06:13 +0100

    March 1 progress

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+/*
 $(document).ready( () => {
-	$('#' + currentPage).addClass("selected").attr("href", "javascript:void(0)");
+	let navigationBarButton = $('#' + currentPage);
+	if (navigationBarButton.length)
+	{
+		navigationBarButton.addClass("selected").attr("href", "javascript:void(0)");
+	}
 });
+*/
